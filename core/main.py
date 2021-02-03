1872cf38da0a353f817c084a397e60389268b763
8BitJonny <timon99@gmx.de>
2021-02-03 13:36:11 +0100

    feat(core): created program entry point

---
+++ core/main.py
@@ -0,0 +1,15 @@
+def setup():
+    print('--Running Setup')
+    pass
+
+
+def main():
+    print('--Starting')
+    while True:
+        pass
+
+
+if __name__ == "__main__":
+    print('🎯 Dart Recognition 🎯')
+    setup()
+    main()
