4ceae41f716447595359eca2fbc2f7e490b56f9c
8BitJonny <timon99@gmx.de>
2021-03-02 15:50:16 +0100

    fix(core): remove hardcoded ssh identity file

--- core/helper_functions/bash_functions.py
+++ core/helper_functions/bash_functions.py
@@ -4,7 +4,7 @@ import subprocess
 
 def copy_file_to_remote(file_path, remote_dir, ssh_ip):
     return execute_command(
-        f'cat {file_path} | ssh -i ~/.ssh/ssh_pi {ssh_ip} "cat > {remote_dir}/{os.path.basename(file_path)}"'
+        f'cat {file_path} | ssh {ssh_ip} "cat > {remote_dir}/{os.path.basename(file_path)}"'
     )
 
 
@@ -12,13 +12,13 @@ def copy_file_from_remote(remote_file_path, local_file_path, ssh_ip):
     print(remote_file_path)
     filename = os.path.basename(remote_file_path)
     return subprocess.call(
-        f'ssh -i ~/.ssh/ssh_pi {ssh_ip} "cat {remote_file_path}" > {local_file_path}/{filename}',
+        f'ssh {ssh_ip} "cat {remote_file_path}" > {local_file_path}/{filename}',
         shell=True
     )
 
 
 def via_ssh(command, ssh_ip):
-    return f'ssh -i ~/.ssh/ssh_pi {ssh_ip} "{command}"'
+    return f'ssh {ssh_ip} "{command}"'
 
 
 def execute_command(cmd):
