83d3bfbd1b26e388f467c4584a08fabb5373f944
8BitJonny <timon99@gmx.de>
2021-03-02 17:06:55 +0100

    fix(core): return image path correctly in copy_file_from_remote

--- core/helper_functions/bash_functions.py
+++ core/helper_functions/bash_functions.py
@@ -10,10 +10,11 @@ def copy_file_to_remote(file_path, remote_dir, ssh_ip):
 
 def copy_file_from_remote(remote_file_path, local_file_path, ssh_ip):
     filename = os.path.basename(remote_file_path)
-    return subprocess.call(
+    subprocess.call(
         f'ssh {ssh_ip} "cat {remote_file_path}" > {local_file_path}/{filename}',
         shell=True
     )
+    return f'{local_file_path}/{filename}'
 
 
 def via_ssh(command, ssh_ip):
