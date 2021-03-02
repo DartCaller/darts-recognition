f29c2ebc1e2c52045e30d3442716414849da2eba
8BitJonny <timon99@gmx.de>
2021-03-02 17:07:51 +0100

    fix(core): init pi_camera outside of loop

--- core/helper_functions/take_image.py
+++ core/helper_functions/take_image.py
@@ -2,12 +2,13 @@ from picamera import PiCamera
 from time import sleep
 import datetime
 
+camera = PiCamera()
+camera.resolution = (1640, 1232)  # (3280, 2464)
+camera.framerate = 15
+camera.rotation = 180
+
 
 def take_image(target_dir):
-    camera = PiCamera()
-    camera.resolution = (1640, 1232)  # (3280, 2464)
-    camera.framerate = 15
-    camera.rotation = 180
     camera.start_preview(alpha=192)
     sleep(1)
     date = datetime.datetime.now().strftime("%Y-%m-%d_%H:%M:%S")
