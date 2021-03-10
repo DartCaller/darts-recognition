0e6062b2f0ed93a55cb50625aee69aedaef6eeeb
8BitJonny <timon99@gmx.de>
2021-03-10 10:49:49 +0100

    feat(config): fine tune config values

--- core/config.py
+++ core/config.py
@@ -1,6 +1,6 @@
 config = {
     'backend_url': 'localhost:8000',
-    'y_bounds': (700, 1160),
-    'image_diff_threshold': 1000,
-    'pixel_diff_threshold': 0.2
+    'y_bounds': (600, 1200),
+    'image_diff_threshold': 1500,
+    'pixel_diff_threshold': 0.3
 }
