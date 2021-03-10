14b3d8fb5b50f3593441ad567134bce60e91dce1
8BitJonny <timon99@gmx.de>
2021-03-10 17:30:56 +0100

    feat(core): add make_labeled_test_images mode to server

--- core/server.py
+++ core/server.py
@@ -1,51 +1,85 @@
 # https://picamera.readthedocs.io/en/release-1.12/recipes1.html#capturing-to-a-network-stream
 # served as a basis for this file
-import io
+import sys
 import socket
 import struct
 from helper_functions import save_image
 import cv2
 import numpy as np
 import main
+from pathlib import Path
+from threading import Thread
+from collections import deque
+from time import sleep
+
+
+main.setup()
+server_socket = socket.socket()
+server_socket.bind(('0.0.0.0', 8000))
+server_socket.listen(10)
+received_image_queue = deque()
+print('Listening for Ws Connection')
+
+# Accept a single connection and make a file-like object out of it
+connection = server_socket.accept()[0].makefile('rb')
 
 
 def convert_jpeg_bytes_into_numpy_rgb(jpeg_bytes):
     return cv2.imdecode(np.frombuffer(jpeg_bytes, np.uint8), -1).copy()
 
 
-main.setup()
-# Start a socket listening for connections on 0.0.0.0:8000 (0.0.0.0 means
-# all interfaces)
-server_socket = socket.socket()
-server_socket.bind(('0.0.0.0', 8000))
-server_socket.listen(0)
-print('Listening for Ws Connection')
+def label_image(images_in_bytes):
+    label = input('Label for the current Image Pair:')
+    folder = Path(f'./labeled_images/{label}')
+    folder.mkdir(parents=True, exist_ok=True)
+    count_of_same_label = round(len(list(folder.glob('*.jpg'))) / 2)
+    for i, axs in enumerate(['x', 'y']):
+        with open(f'{str(folder)}/{axs}_{count_of_same_label}.jpg', 'wb') as file:
+            file.write(images_in_bytes[i])
 
-# Accept a single connection and make a file-like object out of it
-connection = server_socket.accept()[0].makefile('rb')
 
-try:
-    while True:
-        # Read the lengths of the images as a 32-bit unsigned int
-        image_lengths = list(map(
-            lambda _: struct.unpack('<L', connection.read(struct.calcsize('<L')))[0],
-            range(2)
-        ))
-        if not any(image_lengths):
-            print('Received Close signal')
-            break
-        print(image_lengths)
-        # Construct streams to hold the image data and read the images
-        # data from the connection
-        image_streams = list(map(lambda _: io.BytesIO(), range(2)))
-        images = []
-        for image_byte_size, axis in zip(image_lengths, ['x', 'y']):
-            image_bytes = connection.read(image_byte_size)
-            save_image(axis, image_bytes)
-            images.append(convert_jpeg_bytes_into_numpy_rgb(image_bytes))
+def read_images_from_ws(image_queue):
+    try:
+        while True:
+            # Read the lengths of the images as a 32-bit unsigned int
+            image_lengths = list(map(
+                lambda _: struct.unpack('<L', connection.read(struct.calcsize('<L')))[0],
+                range(2)
+            ))
+            if not any(image_lengths):
+                print('Received Close signal')
+                break
+            # Construct streams to hold the image data and read the images
+            # data from the connection
+            images_bytes = list(map(
+                lambda image_length: connection.read(image_length),
+                image_lengths
+            ))
 
-        main.on_incoming_images(images[0], images[1])
+            image_queue.append(images_bytes)
 
-finally:
-    connection.close()
-    server_socket.close()
+    finally:
+        connection.close()
+        server_socket.close()
+
+
+Thread(target=read_images_from_ws, args=(received_image_queue,)).start()
+
+while True:
+    try:
+        image_bytes_list = received_image_queue.popleft()
+        if len(sys.argv) > 1 and sys.argv[1] == '--label-mode':
+            label_image(image_bytes_list)
+            input('Enter to continue:')
+            sleep(3)
+            received_image_queue.clear()
+        else:
+            for im_bytes, axis in zip(image_bytes_list, ['x', 'y']):
+                save_image(axis, im_bytes)
+
+            images = list(map(lambda image_bytes: convert_jpeg_bytes_into_numpy_rgb(image_bytes), image_bytes_list))
+            main.on_incoming_images(images[0], images[1])
+    except IndexError:
+        sleep(1)
+    except OSError:
+        pass
