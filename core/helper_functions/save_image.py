0d2a90d22b95faf79256dd427f4fca42e488f9de
8BitJonny <timon99@gmx.de>
2021-03-10 10:46:25 +0100

    fix(core): fix image export filename

--- core/helper_functions/save_image.py
+++ core/helper_functions/save_image.py
@@ -9,5 +9,5 @@ def save_image(axis, stream):
 
 
 def save_image_daemon(axis, stream):
-    with open(f'./pi_images/{axis}/{datetime.now().strftime("%Y-%m-%d_%H:%M:%S")}.jpeg', 'wb') as file:
+    with open(f'./pi_images/{axis}/{datetime.now().strftime("%Y-%m-%dT%H-%M-%S")}.jpeg', 'wb') as file:
         file.write(stream)
