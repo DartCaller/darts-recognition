ed2608eda889a07d3eda61dad76201125e5a4061
8BitJonny <timon99@gmx.de>
2021-04-22 11:53:42 +0200

    refactor(camera): cleanup camera after dartboard_images intro

--- core/camera.py
+++ core/camera.py
@@ -1,42 +1,25 @@
-from helper_functions.binary_diff_images import binary_diff_images
 from config import config
-import numpy as np
-
-
-def get_image_diff(base_image, image):
-    binary_diff_image = binary_diff_images(
-        base_image,
-        image,
-        config['y_bounds'],
-        config['pixel_diff_threshold']
-    )
-    return np.count_nonzero(np.asarray(binary_diff_image) == 255)
 
 
 class Camera:
-    def __init__(self, axis, pos):
+    def __init__(self, axis, pos, y_bounds_min):
         self.last_taken_image = None
         self.empty_board_image = None
         self.pos = pos
         self.axis = axis
+        self.y_bounds_min = 0
+        self.y_bounds_max = 0
+        self.set_y_bounds_min(y_bounds_min)
+
+    def set_y_bounds_min(self, y_bounds_min):
+        self.y_bounds_min = y_bounds_min
+        self.y_bounds_max = y_bounds_min + config['y_bounds_height']
+
+    def crop_image(self, image):
+        return image[self.y_bounds_min:self.y_bounds_max, :]
 
     def update_last_taken_image(self, new_last_image):
         self.last_taken_image = new_last_image
 
     def set_empty_dart_board_image(self, empty_board_image):
         self.empty_board_image = empty_board_image
-
-    def does_image_contain_change(self, image):
-        if self.last_taken_image is None:
-            return False
-
-        number_of_changed_pixels = get_image_diff(self.last_taken_image, image)
-        return number_of_changed_pixels >= config['image_diff_threshold']
-
-    def is_image_empty_board(self, image):
-        if [x for x in (image, self.empty_board_image) if x is None]:
-            return False
-
-        number_of_changed_pixels = get_image_diff(self.empty_board_image, image)
-
-        return number_of_changed_pixels <= config['image_diff_threshold']
