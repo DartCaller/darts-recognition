3f6047fe2a89930850516ed3fac20fe2661b1b7a
8BitJonny <timon99@gmx.de>
2021-04-22 11:32:00 +0200

    feat(read_image): catch potential file not found error

--- core/helper_functions/read_image.py
+++ core/helper_functions/read_image.py
@@ -1,5 +1,9 @@
 import cv2
+import os.path
 
 
 def read_image(image_path):
-    return cv2.imread(image_path)
+    if os.path.isfile(image_path):
+        return cv2.imread(image_path)
+    else:
+        raise FileNotFoundError(f'{image_path} not found')
