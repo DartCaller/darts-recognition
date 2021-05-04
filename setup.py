de25f1843096e6247d886aa96bb65b3ad14cda98
8BitJonny <timon99@gmx.de>
2021-05-04 19:32:52 +0200

    fix(build): fix cython build conf

--- setup.py
+++ setup.py
@@ -4,7 +4,6 @@ import numpy as np
 
 setup(
     name='Darts Recognition',
-    package_dir={'': 'core'},
     ext_modules=cythonize(
         "core/helper_functions/binary_diff_images.pyx",
         language_level="3",
